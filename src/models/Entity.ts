7af6501b9c529324eda5032006ca5e0ab338ae49
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-02-24 19:24:23 -0600

    Update Entity.ts

--- src/models/Entity.ts
+++ src/models/Entity.ts
@@ -3,7 +3,7 @@ import { types } from 'mobx-state-tree';
 import Block, { IBlock } from 'src/models/Block';
 import Door, { IDoor } from 'src/models/Door';
 import Hoppi, { IHoppi } from 'src/models/Hoppi';
-import Text, { IText } from 'src/models/Text;
+import Text, { IText } from 'src/models/Text';
 
 const Entity = types.union(Block, Door, Hoppi, Text);
 export default Entity;

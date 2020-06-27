447230b6bceabbf8263ec71deb665e3c4f30e9b6
Julien Mourer <getkey@getkey.eu>
2020-06-27 20:54:57 +0200

    Replace reset angle button with a number input

--- src/components/AngleParam.tsx
+++ src/components/AngleParam.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent, ChangeEvent } from 'react';
+import styled from '@emotion/styled';
 import { observer } from 'mobx-react-lite';
 import { DEG_TO_RAD, RAD_TO_DEG } from 'pixi.js';
 
@@ -6,13 +7,18 @@ import { useStore } from 'src/hooks/useStore';
 import Hoppi from 'src/models/Hoppi';
 import Door from 'src/models/Door';
 import Text from 'src/models/Text';
+import NumberInput from 'src/components/NumberInput';
+
+const AngleInput = styled(NumberInput)`
+	width: 6ex;
+`;
 
 const AngleParam: FunctionComponent = () => {
 	const { editor: { selectedEntity } } = useStore();
 
 	if (!(Hoppi.is(selectedEntity) || Door.is(selectedEntity) || Text.is(selectedEntity)) || selectedEntity === undefined) return null;
 
-	function onChangeAngle(ev: ChangeEvent<HTMLInputElement>): void {
+	function onChangeAngleSlider(ev: ChangeEvent<HTMLInputElement>): void {
 		if (selectedEntity === undefined) return;
 		if (!Hoppi.is(selectedEntity) && !Door.is(selectedEntity) && !Text.is(selectedEntity)) {
 			throw new Error('Neither door, Hoppi nor text');
@@ -20,25 +26,37 @@ const AngleParam: FunctionComponent = () => {
 		selectedEntity.setAngle(ev.target.valueAsNumber * DEG_TO_RAD);
 	}
 
-	function onResetAngle(): void {
+	function onChangeAngleInput(angle: number): void {
 		if (selectedEntity === undefined) return;
 		if (!Hoppi.is(selectedEntity) && !Door.is(selectedEntity) && !Text.is(selectedEntity)) {
 			throw new Error('Neither door, Hoppi nor text');
 		}
-		selectedEntity.setAngle(0);
+		selectedEntity.setAngle(angle * DEG_TO_RAD);
 	}
+
+	// remove floating point inaccuracies by flooring
+	const angleDegrees = Math.floor(selectedEntity.params.angle * RAD_TO_DEG);
 
 	return (
 		<div>
-			<label>angle: <input
+			<label>
+				angle:
+				<AngleInput
+					min="-180"
+					max="180"
+					step="1"
+					value={angleDegrees}
+					onChange={onChangeAngleInput}
+				/>
+			</label>
+			<input
 				type="range"
 				min="-180"
 				max="180"
 				step="1"
-				onChange={onChangeAngle}
-				value={selectedEntity.params.angle * RAD_TO_DEG}
-			/></label>
-			<button onClick={onResetAngle}>Reset angle</button>
+				value={angleDegrees}
+				onChange={onChangeAngleSlider}
+			/>
 		</div>
 	);
 };

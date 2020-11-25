a230e98cc2d5d158f38dcfc061ed3b8668577edd
Julien Mourer <getkey@getkey.eu>
2020-11-25 19:58:56 +0100

    Deploy to S3

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 	devtool: isProd ? 'source-map' : 'eval-source-map',
 	entry: './src/index.tsx',
 	output: {
-		filename: 'js/[name].[hash].js',
+		filename: 'js/[name].[contenthash].js',
 		path: path.resolve(__dirname, 'dist'),
 	},
 	optimization: {

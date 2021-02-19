dff1df305ac520e6574debdb97186d81197e623c
Julien Mourer <getkey@getkey.eu>
2021-02-19 23:57:34 +0100

    Add warning when you are creating a circle so big that it might crash the editor

--- src/models/RadiusParams.ts
+++ src/models/RadiusParams.ts
@@ -10,16 +10,28 @@ const RadiusParams  = types.model({
 		),
 		15,
 	),
-}).actions((self) => ({
-	setRadius(radius: number): void {
-		if (radius <= 0) {
-			(getParent(self) as RadiusParamsEntity).remove();
-			return;
-		}
+}).actions((self) => {
+	let hasBeenWarned = false;
 
-		self.radius = radius;
-	},
-}));
+	return {
+		setRadius(radius: number): void {
+			if (radius <= 0) {
+				(getParent(self) as RadiusParamsEntity).remove();
+				return;
+			}
+
+			if (!hasBeenWarned && radius > 5000) {
+				if (window.confirm('Creating such a big circle can crash the editor. Take the risk?')) {
+					hasBeenWarned = true;
+				} else {
+					return;
+				}
+			}
+
+			self.radius = radius;
+		},
+	};
+});
 
 export default RadiusParams;
 // eslint-disable-next-line @typescript-eslint/no-empty-interface

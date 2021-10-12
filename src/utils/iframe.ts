952d9053d12b9b63642a87aeb1c050f739ddb6ce
Julien Mourer <getkey@getkey.eu>
2021-10-12 18:12:14 +0200

    Whitelist the Poki-hosted domain for sending editor commands

--- src/utils/iframe.ts
+++ src/utils/iframe.ts
@@ -6,7 +6,10 @@ type EditorMessage = {
 };
 
 export function postMessage(message: EditorMessage): void {
-	window.parent.postMessage(message, 'https://bombhopper.io');
-	window.parent.postMessage(message, 'https://staging.bombhopper.io');
-	window.parent.postMessage(message, 'http://127.0.0.1:10001');
+	[
+		'https://bombhopper.io',
+		'https://b11df0e8-fbf1-11e9-ab1b-e6a2878b8862.poki-gdn.com',
+		'https://staging.bombhopper.io',
+		'http://127.0.0.1:10001',
+	].forEach((targetOrigin) => window.parent.postMessage(message, targetOrigin));
 }

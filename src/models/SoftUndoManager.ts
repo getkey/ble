6f3797dde17b0fd5489e45220a7705d7ca294aee
Julien Mourer <getkey@getkey.eu>
2022-05-22 22:40:18 +0200

    Refactor the SoftUndoManager

--- src/models/SoftUndoManager.ts
+++ src/models/SoftUndoManager.ts
@@ -1,4 +1,3 @@
-import { types } from 'mobx-state-tree';
 import { UndoManager } from 'mst-middlewares';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
@@ -6,44 +5,28 @@ const noOp = () => {};
 
 // blur events happen after pointerdown events, which makes it sometimes impossible to close the group
 // at the right time. This model will automatically close groups if needed, and make sure not to close already closed groups
-const SoftUndoManager = types.model({
-	undoManager: types.optional(UndoManager, {}),
-	inGroup: false,
-}).actions((self) => ({
-	withoutUndo<T>(fn: () => T): T {
-		return self.undoManager.withoutUndo(fn);
-	},
-})).actions((self) => ({
-	stopGroup() {
-		if (self.inGroup) { // sometimes startGroup will already have stopped a running group, in which case we do nothing
-			self.undoManager.stopGroup();
-			self.withoutUndo(() => {
-				self.inGroup = false;
-			});
-		}
-	},
-})).actions((self) => ({
-	startGroup() {
-		self.stopGroup();
+const SoftUndoManager = UndoManager.actions((self) => {
+	let inGroup = false;
+	const { stopGroup: oldStopGroup, startGroup: oldStartGroup } = self;
 
-		self.undoManager.startGroup(noOp);
-		self.undoManager.withoutUndo(() => {
-			self.inGroup = true;
-		});
-	},
-	undo(): void {
-		self.undoManager.undo();
-	},
-	redo(): void {
-		self.undoManager.redo();
-	},
-})).views((self) => ({
-	get canUndo() {
-		return self.undoManager.canUndo;
-	},
-	get canRedo() {
-		return self.undoManager.canRedo;
-	},
-}));
+	function stopGroup() {
+		// sometimes startGroup will already have stopped a running group, in which case we do nothing
+		if (!inGroup) return;
+
+		oldStopGroup();
+		inGroup = false;
+	}
+	function startGroup() {
+		stopGroup();
+
+		oldStartGroup(noOp);
+		inGroup = true;
+	}
+
+	return {
+		stopGroup,
+		startGroup,
+	};
+});
 
 export default SoftUndoManager;

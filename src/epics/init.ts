de54a16d995d1837a6e9a2e53acd14753f7e8d17
Julien Mourer <getkey@getkey.eu>
2022-05-25 13:05:02 +0200

    Upgrade dependencies

--- src/epics/init.ts
+++ src/epics/init.ts
@@ -1,6 +1,6 @@
 import { EMPTY } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-import { ignoreElements, pluck, tap, filter } from 'rxjs/operators';
+import { ignoreElements, pluck, tap } from 'rxjs/operators';
 import { Epic } from 'epix';
 import { applySnapshot } from 'mobx-state-tree';
 import { getBounds } from 'bombhopperio-level-tools';
@@ -9,12 +9,12 @@ export const loadLevelFromQueryParam: Epic = (action$, { store }) => {
 	const urlParams = new URLSearchParams(window.location.search);
 	const levelId = urlParams.get('levelId');
 
-	if (levelId) {
+	if (levelId && window.confirm('The saved level will be overwritten. Are you sure?')) {
 		return ajax(`https://assets.bombhopper.io/levels/${levelId}.json`).pipe(
 			pluck('response'),
-			filter(() => window.confirm('The saved level will be overwritten. Are you sure?')),
 			tap((level) => {
-				const { top, right, bottom, left } = getBounds(level.entities);
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
+				const { top, right, bottom, left } = getBounds((level as any).entities);
 				const middle = {
 					x: left + (right - left) / 2,
 					y: top + (bottom - top) / 2,

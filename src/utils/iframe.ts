b5d3d244e0803bd6d6a9df10814fb10286032df0
Julien Mourer <getkey@getkey.eu>
2026-03-01 23:05:25 +0100

    Allow sending postMessage to any parent
    Poki recently changed the domain on which the game is hosted, which broke the editor. There is no reason to whitelist the `postMessage`s, and not doing it makes the editor more future-proof.

--- src/utils/iframe.ts
+++ src/utils/iframe.ts
@@ -6,10 +6,5 @@ type EditorMessage = {
 };
 
 export function postMessage(message: EditorMessage): void {
-	[
-		'https://bombhopper.io',
-		'https://b11df0e8-fbf1-11e9-ab1b-e6a2878b8862.poki-gdn.com',
-		'https://staging.bombhopper.io',
-		'http://127.0.0.1:10001',
-	].forEach((targetOrigin) => window.parent.postMessage(message, targetOrigin));
+	window.parent.postMessage(message, '*');
 }
